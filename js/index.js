4e13ce1828c6ba79c7aa108a5f141401e9119a88
Codexgrey <thecodexgrey@gmail.com>
2021-09-21 09:57:34 +0100

    more changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // javascript for index.html
+
+const renderPosts = async () => {
+    let url =  'http://localhost:3000/posts';
+
+    const res = await fetch(url);
+    const posts = await res.json();
+    
+    let template = '';
+    posts.map(post => {
+       template += `
+       <div class="post">
+        <h2>${post.title}</h2>
+        <p><small> ${post.likes} likes <small></P>
+        <p>${post.body.slice(0, 200)}</p>
+        <a href="/details.html">read more...</a>
+       </div>
+       ` ;
+    })
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
