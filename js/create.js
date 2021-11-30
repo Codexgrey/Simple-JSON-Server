7042c25e5e3d431549a281e8c94c7f3cb8a186ee
Codexgrey <thecodexgrey@gmail.com>
2021-11-30 07:27:18 +0100

    json

--- js/create.js
+++ js/create.js
@@ -9,7 +9,7 @@ const createpost = async (e) => {
         likes: 0
     }
 
-    await fetch('http://localhost:8000/posts', {
+    await fetch('http://localhost:3000/posts', {
         method: 'POST',
         body: JSON.stringify(newPost),
         headers: { 'content-type': 'application/json' } // this is to say we're sending a json file
